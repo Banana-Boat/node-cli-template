6257415cbcff6d59069005f5c04661d8bcf7720a
bananaboat <937499953@qq.com>
2023-03-19 11:36:59 +0800

    Drop files from .gitignore

--- bin/handlers.js
+++
@@ -1,5 +0,0 @@
-import chalk from "chalk";
-export const showTimeHandler = (argv) => {
-    console.log(argv);
-    console.log(chalk.underline.italic.greenBright(`Hello ${argv.name}, it is ${new Date().toLocaleTimeString()} now!`));
-};
